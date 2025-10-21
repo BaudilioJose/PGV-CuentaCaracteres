aaf378c3423939ca88485781a4f5500b2756022b
BaudilioJose <usr2216@salesianos-lacuesta.net>
2025-10-21 11:05:36 +0100

    feat: enhance argument handling and word count functionality

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        if (args.length == 0) {
+            System.out.println("No se ha proporcionado ningún argumento");
+            return;
+        }
+
+        String textLine = args[0];
+
+        if (textLine.trim().isEmpty()) {
+            System.out.println("El argumento no puede estar vacío");
+            return;
+        }
+
+        String[] words = textLine.trim().split("\\s+");
+
+        System.out.println("El número de palabras es: " + words.length);
+
+        
+       
     }
 }
